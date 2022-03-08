c88b2c774b364a52259af96109a4edac7414e701
isaac solomon <49633972+IsaacSolomon@users.noreply.github.com>
2022-03-08 18:12:24 +0530

    chore: navbar ui (#3)

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,1 +1,19 @@
-Nav
+<div class="bg-white md:bg-transparent md:max-w-7xl md:mx-auto md:relative flex items-center align-center justify-between px-6 py-4 md:pt-20">
+  <a href="javascript:void(0)" class="inline-block md:absolute md:right-[62px]">
+    <img src="/assets/hamburger-menu.svg" alt="">
+  </a>
+  <a href="javascript:void(0)" class="inline-block">
+    <p class="text-20 font-medium text-black">Keus Home</p>
+  </a>
+  <a href="javascript:void(0)" class="inline-block md:absolute md:right-6">
+    <img src="/assets/calendar.svg" alt="">
+  </a>
+
+  <ul class="items-center align-center absolute top-[62px] md:top-auto right-0 md:right-auto left-0 md:left-auto bottom-0 md:bottom-auto bg-white md:bg-transparent md:relative md:space-x-10 md:pr-40 hidden md:flex flex-col md:flex-row z-10 justify-center text-[32px] md:text-[16px] space-y-4 md:space-y-0">
+    <li><a href="javascript:void(0)">Home</a></li>
+    <li><a href="javascript:void(0)">Consoles</a></li>
+    <li><a href="javascript:void(0)">Hub</a></li>
+    <li><a href="javascript:void(0)">Wisard</a></li>
+    <li><a href="javascript:void(0)">Sensors</a></li>
+  </ul>
+</div>
